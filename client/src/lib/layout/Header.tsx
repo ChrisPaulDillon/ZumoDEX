8becf0fc8ce71ba46cf72333cd08dd0bbef515a5
Chris <chrispauldillon@live.com>
2022-05-04 22:01:17 +0100

    Added company logo, removed boilerplate images

--- client/src/lib/layout/Header.tsx
+++ client/src/lib/layout/Header.tsx
@@ -1,12 +1,15 @@
 import { Box, Flex, Heading } from "@chakra-ui/react";
 import WalletConnectButton from "lib/components/WalletConnectButton";
 import Link from "next/link";
-
+import Image from "next/image";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
     <Flex as="header" width="full" align="center">
+      <Box pr={2}>
+        <Image src="/zumo-mobile-logo.svg" height={30} width={30} alt="Zumo" />
+      </Box>
       <Heading as="h1" size="md">
         <Link href="/">ZumoSwap!</Link>
       </Heading>
