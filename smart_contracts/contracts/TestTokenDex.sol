1ce34b3d8cb3b0309be4a0484f0b70f37c87f8b4
Chris <chrispauldillon@live.com>
2022-05-07 14:18:09 +0100

    Fixed decimals on erc20 contract

--- smart_contracts/contracts/TestTokenDex.sol
+++ smart_contracts/contracts/TestTokenDex.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestTokenDex is ERC20 {
-    uint256 constant initialSupply = 500000000000000 * (10**2);
+    uint256 constant _initial_supply = 500000000000000 * (10**2);
 
     constructor() ERC20("TestTokenDex", "TTD") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, _initial_supply);
+    }
+
+     function decimals() public view virtual override returns (uint8) {
+        return 2;
     }
 }
