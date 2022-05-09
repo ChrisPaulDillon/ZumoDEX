3ba9a078db1cf0b063f14f20e4e523bf518000de
Chris <chrispauldillon@live.com>
2022-05-09 08:26:10 +0100

    Fixed balance formatting

--- client/util/balanceHelper.ts
+++ client/util/balanceHelper.ts
@@ -1,4 +1,5 @@
 import BigNumber from "bignumber.js";
+import { BigNumberish, ethers } from "ethers";
 
 export const BIG_TEN = new BigNumber(10);
 
@@ -8,4 +9,9 @@ export const getBalanceNumber = (balance: BigNumber, decimals = 18) => {
 
 export const getBalanceAmount = (amount: BigNumber, decimals = 18) => {
   return new BigNumber(amount).dividedBy(BIG_TEN.pow(decimals));
+};
+
+export const getTokenBalance = (amount: BigNumberish, decimals = 2): Number => {
+  const result = (ethers.utils.formatUnits(amount, decimals));
+  return Number(result)
 };
