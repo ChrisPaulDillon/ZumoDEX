090b66a7943eff3ccb0bbda5ca0123e588d679a6
Chris <chrispauldillon@live.com>
2022-05-09 10:37:15 +0100

    Dark mode is now default colour option

--- client/src/lib/styles/customTheme/index.ts
+++ client/src/lib/styles/customTheme/index.ts
@@ -4,9 +4,15 @@ import colors from "./colors";
 import Button from "./components/button";
 import fonts from "./fonts";
 
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false
+}
+
 const customTheme = extendTheme({
   fonts,
   colors,
+  config,
   components: {
     Button,
   },
