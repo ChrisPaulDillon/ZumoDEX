c4320bb608786f7042418fff5d313d19e909c6cf
Chris <chrispauldillon@live.com>
2022-05-09 16:20:33 +0100

    User is now prompted to install a web3 wallet if none is detected when clicking the connect wallet button

--- client/src/lib/components/WalletConnectButton.tsx
+++ client/src/lib/components/WalletConnectButton.tsx
@@ -1,19 +1,29 @@
 import { ButtonProps, Button } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
+import useFireToast from "../../../hooks/useFireToast";
 import useWalletConnect from "../../../hooks/useWalletConnect";
 import { IAppState } from "../../../state";
+import { CONNECTOR_TYPE } from "../../../state/reducer";
 import { getAbbreviatedAddress } from "../../../util/addressHelper";
 
 const WalletConnectButton: React.FC<ButtonProps> = ({ ...rest }) => {
-  const userAddress = useSelector(
-    (state: IAppState) => state.state.userAddress
-  );
+  const userAddress = useSelector((state: IAppState) => state.state.userAddress);
   const isLoggedIn = useSelector((state: IAppState) => state.state.isLoggedIn);
+  const connectorStatus = useSelector((state: IAppState) => state.state.connectorStatus);
+  const toast = useFireToast();
+  const { login, logout } = useWalletConnect();
 
-  const { active, login, logout, account } = useWalletConnect();
+  const handleWalletConnect = () => {
+    if (connectorStatus === CONNECTOR_TYPE.JSON_RPC) {
+      toast.Negative("Erro", "No Web3 wallet detected, please install MetaMask");
+    } else if (isLoggedIn) {
+      return logout();
+    }
+    return login();
+  };
 
   return (
-    <Button onClick={isLoggedIn ? logout : login} {...rest}>
+    <Button onClick={handleWalletConnect} {...rest}>
       {isLoggedIn ? getAbbreviatedAddress(userAddress!) : "Connect Wallet"}
     </Button>
   );
