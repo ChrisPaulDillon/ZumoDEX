d437675c5909f29aa33e237e0f9a864a1a4c4804
Chris <chrispauldillon@live.com>
2022-05-09 16:23:48 +0100

    Improved folder structure

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,38 @@
-import Home from "lib/pages/home";
+import { Box, Stack } from "@chakra-ui/react";
+import SwapCard from "lib/components/SwapCard";
+import TDDStats from "lib/components/TDDStats";
+import { CONTRACT_ERC20, CONTRACT_DEXSWAP } from "../../contracts/contracts";
+import useBalanceOf from "../../contracts/hooks/useBalanceOf";
+import useGetDexInfo from "../../contracts/hooks/useGetDexInfo";
+import useGetEthBalance from "../../contracts/hooks/useGetEthBalance";
+import useGetTokenInfo from "../../contracts/hooks/useGetTokenInfo";
+import useDetectWalletStatus from "../../hooks/useDetectWalletStatus";
+
+const Home = () => {
+  const balance = useBalanceOf(CONTRACT_ERC20);
+  const tokenInfo = useGetTokenInfo(CONTRACT_ERC20);
+  const dexInfo = useGetDexInfo(CONTRACT_DEXSWAP);
+  useGetEthBalance();
+  useDetectWalletStatus();
+
+  return (
+    <Box
+      display={{ md: "flex" }}
+      alignItems="center"
+      justifyContent={"center"}
+      minHeight="70vh"
+      gap={14}
+      mb={8}
+      w="full"
+    >
+      <Stack spacing={12}>
+        <TDDStats tokenInfo={tokenInfo} userBalance={balance} />
+        <Stack direction={["column", "row"]} spacing={10}>
+          <SwapCard dexInfo={dexInfo} />
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
 
 export default Home;
