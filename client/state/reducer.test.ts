22cf820db6894336547f986807c49a5d601c0993
Chris <chrispauldillon@live.com>
2022-05-10 11:22:29 +0100

    Wrote additional reducer tests

--- client/state/reducer.test.ts
+++ client/state/reducer.test.ts
@@ -1,11 +1,36 @@
-import { initialState, logUserIn, logUserOut, updateEtherBalance } from "./reducer";
+import {
+  CONNECTOR_TYPE,
+  initialState,
+  logUserIn,
+  logUserOut,
+  updateEtherBalance,
+  updateJsonRpcConnection,
+  updateWeb3Provider,
+} from "./reducer";
 import reducer from "./reducer";
+import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import { generateTestingUtils } from "eth-testing";
 
 const testAddress = "0xb794f5ea0ba39494ce839613fffba74279579268";
 const testEtherBalance = 10000000000000000;
 
 describe("Redux Reducer", () => {
+  const testingUtils = generateTestingUtils({ providerType: "MetaMask" });
+  const jsonRpcProvider = new JsonRpcProvider();
+  let web3Provider: Web3Provider;
+
+  beforeAll(() => {
+    //@ts-ignore
+    global.window.ethereum = testingUtils.getProvider();
+    //@ts-ignore
+    web3Provider = new Web3Provider(global.window.ethereum);
+  });
+
   test("should return the initial state", () => {
+    //@ts-ignore
+    const test = new ethers.providers.Web3Provider(window.ethereum);
+    console.log(test);
     expect(reducer(undefined, { type: "" })).toEqual(initialState);
   });
 
@@ -19,6 +44,18 @@ describe("Redux Reducer", () => {
     const state = reducer(initialState, logUserOut());
     expect(state.isLoggedIn).toEqual(false);
     expect(state.userAddress).toEqual(undefined);
+  });
+
+  test("should update the user state to be logged out", () => {
+    const state = reducer(initialState, updateWeb3Provider({ web3Provider: web3Provider }));
+    expect(state.connectorStatus).toEqual(CONNECTOR_TYPE.WALLET_CONNECT);
+    expect(state.web3Provider).toEqual(web3Provider);
+  });
+
+  test("should update the connection status to json RPC", () => {
+    const state = reducer(initialState, updateJsonRpcConnection({ jsonRpcConnector: jsonRpcProvider }));
+    expect(state.connectorStatus).toEqual(CONNECTOR_TYPE.JSON_RPC);
+    expect(state.jsonRpcProvider).toEqual(jsonRpcProvider);
   });
 
   test("should update the users ethereum balance", () => {
